3e6afd453e9a234b61767da08918f7332d33b927
Projects <arthur.franchetto@gmail.com>
2016-07-13 22:43:43 -0300

    First Activity Model

--- app/src/main/java/com/example/arthurf/tcc/app/LoginActivity.java
+++ app/src/main/java/com/example/arthurf/tcc/app/LoginActivity.java
@@ -2,6 +2,8 @@ package com.example.arthurf.tcc.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText etUsuario = (EditText) findViewById(R.id.edUsuario);
+        final EditText etSenha = (EditText) findViewById(R.id.edSenha);
+
+        final Button bAcessar = (Button) findViewById(R.id.btAcessar);
+
     }
 }
