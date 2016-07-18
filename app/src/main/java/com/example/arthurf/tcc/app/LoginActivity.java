4d2a4cf0ddaad96974860a488d40a48ae71edfa8
Projects <arthur.franchetto@gmail.com>
2016-07-18 12:26:38 -0300

    Login Update 1

--- app/src/main/java/com/example/arthurf/tcc/app/LoginActivity.java
+++ app/src/main/java/com/example/arthurf/tcc/app/LoginActivity.java
@@ -1,9 +1,17 @@
 package com.example.arthurf.tcc.app;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.android.volley.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,5 +25,50 @@ public class LoginActivity extends AppCompatActivity {
 
         final Button bAcessar = (Button) findViewById(R.id.btAcessar);
 
+        bAcessar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final String usuario = etUsuario.getText().toString();
+                final String senha = etSenha.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if(success){
+
+                                String nome = jsonResponse.getString("name");
+
+                                Intent intent = new Intent(LoginActivity.this, WelcomeActivity.class);
+
+                                intent.putExtra("nome", nome);
+
+                            }else{
+
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage("Login Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                LoginRequest loginRequest = new LoginRequest(usuario, senha, responseListener);
+            }
+        });
+
     }
+
 }
